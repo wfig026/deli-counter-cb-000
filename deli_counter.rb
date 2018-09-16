fccaa5bd07c1415ca945222d0a92615cff51eb3c
Will Figueiredo <will@wfigueiredo.com>
2018-09-16 18:03:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,12 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
+  else
+    person_with_number = []
+    katz_deli.each_with_index do |name, index|
+      person_with_number.push("#{index + 1}. #{name}")
   end
+  puts "The line is currently: #{person_with_number.join(' ')}"
 end
 
 def take_a_number(katz_deli, name)
