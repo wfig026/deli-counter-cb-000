1ba072dd038ace7b4d6efcecb70a0dc4aaaad98a
Will Figueiredo <will@wfigueiredo.com>
2018-09-16 18:01:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,3 +6,17 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
